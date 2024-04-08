29d6118bb1168f91988e66ca98ba078065ddef48
Eaindra68 <eaindra68.hh@gmail.com>
2024-04-08 17:05:21 +0630

    first commit

---
+++ lib/widgets/appbar_white_corner.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AppBarWhiteCorner extends StatelessWidget {
+  const AppBarWhiteCorner({super.key, this.color});
+  final Color? color;
+
+  static const double kHeight = 30;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 30,
+      decoration: BoxDecoration(
+        color: color ?? Theme.of(context).scaffoldBackgroundColor,
+        borderRadius: const BorderRadius.vertical(
+          top: Radius.circular(30),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Theme.of(context).scaffoldBackgroundColor,
+            offset: const Offset(0, 2),
+          )
+        ],
+      ),
+    );
+  }
+}
